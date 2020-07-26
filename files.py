2772b18edbbd5bd7d358e3e97b4c4df5a1ce40b4
George Doujaiji <georgedouj88@gmail.com>
2020-07-26 19:13:36 -0400

    Add Document and Video child classes

--- files.py
+++ files.py
@@ -8,3 +8,18 @@ class File:
         self.file_name = ntpath.basename(file_path)  # file name extracted from the provided file path
         self.file_path = file_path  # file path
         self.extension = self.file_name[self.file_name.find('.'):] # file extension extracted from the provided file path(.png, .pdf, etc.)
+
+
+class Document(File):
+
+    def __init__(self, file_path: str):
+        super().__init__(file_path)
+
+
+class Video(File):
+
+    def __init__(self, file_path: str):
+        super().__init__(file_path)
+    
+    def watch(self):
+        os.startfile(self.file_path)
